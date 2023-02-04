54c3305fb0d66cbd6a018319cb0f3b3280097b8d
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-02-04 13:59:54 +0200

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+const btnColor = document.querySelector('.change-color');
+const spanText = document.querySelector('.color');
+
+btnColor.addEventListener('click', () => {
+  const colorValue = getRandomHexColor();
+  document.body.style.backgroundColor = colorValue;
+  spanText.textContent = colorValue;
+});
