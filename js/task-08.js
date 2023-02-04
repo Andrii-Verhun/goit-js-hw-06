b2af0e7df4580dd795e5811fd5c22f899fac6214
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-02-04 13:50:22 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const {
+        elements: { email, password }
+    } = evt.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert('Усі поля повинні бути заповненими!');
+    };
+    const newUser = {};
+    newUser.email = email.value;
+    newUser.password = password.value;
+    form.reset();
+    return console.log(newUser);
+});
 
