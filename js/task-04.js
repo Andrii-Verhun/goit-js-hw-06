c5b5d31c2a3b7bdf11aa8a6a53137a5ae4952019
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-02-04 12:03:56 +0200

    Task 4

---
+++ js/task-04.js
@@ -0,0 +1,16 @@
+let counterValue = 0;
+
+const counter = document.querySelector('#value');
+
+const btnDecrement = document.querySelector('button[data-action="decrement"]');
+const btnIncrement = document.querySelector('button[data-action="increment"]');
+
+btnDecrement.addEventListener('click', () => {
+    counterValue -= 1;
+    counter.textContent = counterValue;
+});
+
+btnIncrement.addEventListener('click', () => {
+    counterValue += 1;
+    counter.textContent = counterValue;
+});
